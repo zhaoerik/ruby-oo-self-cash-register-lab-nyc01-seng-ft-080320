b1dc48a7a1a33a2b41eb772ce7f5c4c4a0a2eb0b
Erik Zhao <erikzhao98@gmail.com>
2021-03-08 16:37:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,21 +16,22 @@ class CashRegister
 
     def add_item(title, price, quantity = 1)
         self.total += price * quantity
-        @cart.push(title)
     end
 
-    def apply_discount
-        if self.discount != 0
-            self.total = self.total * ((100-discount.to_f)/100).to_i
-            "After the discount, the total comes to $#{@total}."
-        else
-            "There is no discount to apply."
-    end
+    # def apply_discount
+    #     if self.discount != 0
+    #         self.total = self.total * ((100-discount.to_f)/100).to_i
+    #         "After the discount, the total comes to $#{@total}."
+    #     else
+    #         "There is no discount to apply."
+    #     end
+    # end
 
-    def void_last_transaction
-
-    end
 end
+
+# def items
+#     @cart
+# end
 
 # def void_last_transaction
 #     @cart.shift
